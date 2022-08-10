f7556393312336b6d461aa09c35889643df0fed9
GianlucaFerrari01 <105463604+GianlucaFerrari01@users.noreply.github.com>
2022-08-10 00:09:32 -0300

    Add files via upload

--- testeoCalculadoraSueldo/js/main.js
+++ testeoCalculadoraSueldo/js/main.js
@@ -1,108 +1,97 @@
-alert("Bienvenido al sistema de asistencia al empleado")
-alert("Para realizar una verificación de su sueldo siga las intrucciones:")
+selector()
 
-interfaz()
+function selector() {
 
-function interfaz () {
-    let categoria = prompt(`Ingrese su categoría:
-    INGRESANTE
-    OPERARIO CALIFICADO
-    MEDIO OFICIAL
-    OPERARIO ESPECIALIZADO
-    OPERARIO ESPDO MÚLTIPLE
-    OFICIAL
-    OFICIAL MÚLTIPLE`);
-    
-    let diast = Number(prompt("Días trabajados:"))
-    let hext = Number(prompt("Horas extras trabajadas:"))
-    const hex = (categoria + (categoria / 2))
 
-    calculadora( categoria,diast,hext,hex)
+    const categorias = ["INGRESANTE", "OPERARIO CALIFICADO", "MEDIO OFICIAL", "OPERARIO ESPECIALIZADO", "OPERARIO ESPDO MÚLTIPLE", "OFICIAL", "OFICIAL MÚLTIPLE"];
+
+    let select = document.createElement("select");
+    select.name = "categoria";
+    select.id = "categoria"
 
 }
 
-function calculadora(categoria,diast,hex) {
+/*function diasTrabajados() {
+    const diasT = ""
+
+
+
+    calculadora(diasT)
+}
+
+function horasExtrasTrabajadas() {
+    const hexT = ""
+
+
+
+    calculadora(hexT)
+}
+*/
+function calculadora(categorias, diasT, hexT) {
     let hnorfin;
     let hrexfin;
+    let sueldoxHora;
+    let valorHoraExtra;
     let sueldoNormal;
-    let sueldoMasExtras;
     let sueldoFinal;
-    switch (categoria) {
+    switch (categorias) {
+        
         case 'INGRESANTE': {
-            let sueldoxHoraIn = 434.82
-            let valorHoraExtraIn = 652.23
-            hnorfin = 8.8 * diast
-            hrexfin = valorHoraExtraIn * hex
-            sueldoNormal = sueldoxHoraIn * hnorfin;
-            sueldoMasExtras = sueldoNormal + hrexfin;
-            sueldoFinal = sueldoMasExtras;
-                break;
+            let sueldoxHora = 434.82
+            let valorHoraExtra = 652.23
+            break;
         }
         case 'OPERARIO CALIFICADO': {
-            let sueldoxHoraOc = 470.79;
-            let valorHoraExtraoc = 706.185;
-            hnorfin = 8.8 * diast;
-            hrexfin = valorHoraExtraoc * hex;
-            sueldoNormal = sueldoxHoraOc * hnorfin;
-            sueldoMasExtras = sueldoNormal + hrexfin;
-            sueldoFinal = sueldoMasExtras;
-                break;
+            let sueldoxHora = 470.79;
+            let valorHoraExtra = 706.185;
+            break;
         }
         case 'MEDIO OFICIAL': {
-            let sueldoxHoramo = 508.53;
-            let valorHoraExtramo = 762.79;
-            hnorfin = 8.8 * diast;
-            hrexfin = valorHoraExtramo * hex;
-            sueldoNormal = sueldoxHoramo * hnorfin;
-            sueldoMasExtras = sueldoNormal + hrexfin;
-            sueldoFinal = sueldoMasExtras;
-                break;
+            let sueldoxHora = 508.53;
+            let valorHoraExtra = 762.79;
+            break;
         }
         case 'OPERARIO ESPECIALIZADO': {
-            let sueldoxHoraoe = 543.60;
-            let valorHoraExtraoe = 815.4;
-            hnorfin = 8.8 * diast;
-            hrexfin = valorHoraExtraoe * hex;
-            sueldoNormal = sueldoxHoraoe * hnorfin;
-            sueldoMasExtras = sueldoNormal + hrexfin;
-            sueldoFinal = sueldoMasExtras;
-                break;
+            let sueldoxHora = 543.60;
+            let valorHoraExtra = 815.4;
+            break;
         }
         case 'OPERARIO ESPDO MÚLTIPLE': {
-            let sueldoxHoraoem = 574.83;
-            let valorHoraExtraoem = 926.14;
-            hnorfin = 8.8 * diast;
-            hrexfin = valorHoraExtraoem * hex;
-            sueldoNormal = sueldoxHoraoem * hnorfin;
-            sueldoMasExtras = sueldoNormal + hrexfin;
-            sueldoFinal = sueldoMasExtras;
-                break;
+            let sueldoxHora = 574.83;
+            let valorHoraExtra = 926.14;
+            break;
         }
         case 'OFICIAL': {
-            let sueldoxHorao = 602.41;
-            let valorHoraExtrao = 903.61;
-            hnorfin = 8.8 * diast;
-            hrexfin = valorHoraExtrao* hex;
-            sueldoNormal = sueldoxHorao * hnorfin;
-            sueldoMasExtras = sueldoNormal + hrexfin;
-            sueldoFinal = sueldoMasExtras;
-                break;
+            let sueldoxHora = 602.41;
+            let valorHoraExtra = 903.61;
+            break;
         }
         case 'OFICIAL MÚLTIPLE': {
-            let sueldoxHoraom = 647.80;
-            let valorHoraExtraom = 971.7;
-            hnorfin = 8.8 * diast;
-            hrexfin = valorHoraExtraom * hex;
-            sueldoNormal = sueldoxHoraom * hnorfin;
-            sueldoMasExtras = sueldoNormal + hrexfin;
-            sueldoFinal = sueldoMasExtras;
-                break;
+            let sueldoxHora = 647.80;
+            let valorHoraExtra = 971.7;
+            break;
         }
         default: {
             alert("este código se ejecutará si ninguno de los casos coincide con la expresión")
             break;
         }
     }
-    alert(`Su sueldo final sería: $${sueldoFinal}`)
+    hnorfin = 8.8 * diasT;
+    hrexfin = valorHoraExtra* hexT;
+    sueldoNormal = sueldoxHora * hnorfin;
+    sueldoMasExtras = sueldoNormal + hrexfin;
+    sueldoFinal = sueldoMasExtras;
+
+    salida(sueldoFinal)
+}
+
+function salida (sueldoFinal) {
+
+    const nodoSueldo = document.getElementsByID("resultado");
+
+    for(let nodo of nodoSueldo)
+    {
+        nodo.innerText=`$ ${sueldoFinal}`;
+    }
 
 }
